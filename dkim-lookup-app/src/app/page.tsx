c4c2e036a71bc2f77ef6432cbe0437f62d993c95
Olof Andersson <olof.andersson@gmail.com>
2023-11-29 18:33:10 +0100

    search via searchParams

--- dkim-lookup-app/src/app/page.tsx
+++ dkim-lookup-app/src/app/page.tsx
@@ -1,38 +1,56 @@
-import { PrismaClient, Prisma } from '@prisma/client'
+import { PrismaClient, Prisma, DkimRecord } from '@prisma/client'
 
-export default async function Home() {
+interface DomainSearchResultProps {
+  records: DkimRecord[];
+}
 
+const DomainSearchResults: React.FC<DomainSearchResultProps> = ({ records }) => {
+  return (
+    <div className='dkim-records'>
+      <table>
+        <thead>
+          <tr>
+            <th>Selector</th>
+            <th>Fetched date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {records.map((record) => (
+            <tr key={record.id}>
+              <td className='px-4'>{record.dkimSelector}</td>
+              <td className='px-4'>{record.fetchedAt.toLocaleString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default async function Home({ searchParams }: {
+  searchParams: { [key: string]: string | string[] | undefined }
+}) {
   const prisma = new PrismaClient()
-
-  let records = await prisma.dkimRecord.findMany();
+  const domainQuery = searchParams?.domain?.toString();
+  let records: DkimRecord[] = [];
+  if (domainQuery) {
+    records = await prisma.dkimRecord.findMany({
+      where: {
+        dkimDomain: domainQuery,
+      },
+    })
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <h1 className='p-20 text-xl font-bold'>
+      <h1 className='p-8 text-xl font-bold'>
         DKIM Lookup
       </h1>
-      <div className='dkim-records'>
-        <table>
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Domain</th>
-              <th>Selector</th>
-              <th>Fetched date</th>
-            </tr>
-          </thead>
-          <tbody>
-            {records.map((record) => (
-              <tr key={record.id}>
-                <td>{record.id}</td>
-                <td>{record.dkimDomain}</td>
-                <td>{record.dkimSelector}</td>
-                <td>{record.fetchedAt.toLocaleString()}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      {domainQuery
+        ? records.length > 0
+          ? <div><p className='p-4'>Search results for "{domainQuery}"</p><DomainSearchResults records={records} /></div>
+          : <p>No records found for "{domainQuery}"</p>
+        : <p>Enter a search term</p>}
     </main>
   )
 }
