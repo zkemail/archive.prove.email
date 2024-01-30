89d565ffcb86aa9403bc61e5260a018d27460b91
Olof Andersson <olof.andersson@gmail.com>
2024-01-30 17:51:32 +0100

    extract UploadTsv component

---
+++ dkim-lookup-app/src/app/upload/UploadTsv.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import { load_domains_and_selectors_from_tsv } from "@/lib/tsv";
+import axios from "axios";
+import React from "react";
+import { LogConsole } from "@/components/LogConsole";
+
+export default function Page() {
+
+	const [log, setLog] = React.useState<string[]>([]);
+	const [selectedFile, setSelectedFile] = React.useState<File | undefined>();
+	const [started, setStarted] = React.useState<boolean>(false);
+
+	function fileSelectCallback(event: React.ChangeEvent<HTMLInputElement>) {
+		const file = event.target.files?.[0];
+		setSelectedFile(file);
+	}
+
+	function logmsg(message: string) {
+		console.log(message);
+		setLog(log => [...log, message]);
+	}
+
+	function readFile(file: File) {
+		return new Promise((resolve, reject) => {
+			var fr = new FileReader();
+			fr.onload = () => {
+				resolve(fr.result)
+			};
+			fr.onerror = reject;
+			fr.readAsText(file);
+		});
+	}
+
+	async function uploadFile() {
+		if (!selectedFile) {
+			throw "no file selected";
+		}
+		let fileContent = await readFile(selectedFile);
+		if (!fileContent || (typeof fileContent !== "string")) {
+			throw "error: invalid file content:" + fileContent;
+		}
+
+		let domainSelectorPairs = load_domains_and_selectors_from_tsv(fileContent);
+
+		const upsertApiUrl = 'api/upsert_dkim_record';
+		logmsg(`starting upload to ${upsertApiUrl}`);
+		for (const { domain, selector } of domainSelectorPairs) {
+			logmsg(`uploading ${domain} ${selector}`);
+			await axios.get(upsertApiUrl, { params: { domain, selector } })
+				.then(response => {
+					console.log('response.data: ', response.data);
+					logmsg(`response: ${response.data.message}`);
+				}).catch(error => {
+					console.log(error);
+					let data = error?.response?.data;
+					let message = `${error}` + (data ? ` - ${data}` : "");
+					throw message;
+				})
+		}
+	}
+
+	async function startUpload() {
+		if (!started) {
+			setStarted(true);
+			try {
+				await uploadFile();
+				logmsg("upload complete");
+			}
+			catch (error) {
+				logmsg(`upload failed: ${error}`);
+			}
+			finally {
+				setStarted(false);
+			}
+		}
+	}
+
+	const startEnabled = selectedFile && !started;
+
+	return (
+		<div>
+			<h3>Upload from TSV file</h3>
+			<p>
+				Add records to the database by providing a TSV file with domains and selectors.
+				This page will parse the file and add the records to the database via the <code>api/upsert_dkim_record</code> API.
+			</p>
+			<div>
+				<div>Select a file:</div>
+				<input type="file" onChange={fileSelectCallback} accept=".tsv,.txt" />
+			</div>
+			<p>
+				<button disabled={!startEnabled} onClick={startUpload}>
+					{started ? "Running..." : "Start"}
+				</button>
+			</p>
+			<LogConsole log={log} setLog={setLog} />
+		</div >
+	)
+}
--- dkim-lookup-app/src/app/upload/page.tsx
+++ dkim-lookup-app/src/app/upload/page.tsx
@@ -1,16 +1,11 @@
 "use client";
 
-import { load_domains_and_selectors_from_tsv } from "@/lib/tsv";
-import axios from "axios";
 import React from "react";
 import { useSession, signIn, signOut } from "next-auth/react"
-import { LogConsole } from "@/components/LogConsole";
+import UploadTsv from "./UploadTsv";
 
 export default function Page() {
 
-	const [log, setLog] = React.useState<string[]>([]);
-	const [selectedFile, setSelectedFile] = React.useState<File | undefined>();
-	const [started, setStarted] = React.useState<boolean>(false);
 	const { data: session, status } = useSession()
 
 	if (status == "unauthenticated") {
@@ -23,73 +18,6 @@ export default function Page() {
 		return <p>loading...</p>
 	}
 
-	function fileSelectCallback(event: React.ChangeEvent<HTMLInputElement>) {
-		const file = event.target.files?.[0];
-		setSelectedFile(file);
-	}
-
-	function logmsg(message: string) {
-		console.log(message);
-		setLog(log => [...log, message]);
-	}
-
-	function readFile(file: File) {
-		return new Promise((resolve, reject) => {
-			var fr = new FileReader();
-			fr.onload = () => {
-				resolve(fr.result)
-			};
-			fr.onerror = reject;
-			fr.readAsText(file);
-		});
-	}
-
-	async function uploadFile() {
-		if (!selectedFile) {
-			throw "no file selected";
-		}
-		let fileContent = await readFile(selectedFile);
-		if (!fileContent || (typeof fileContent !== "string")) {
-			throw "error: invalid file content:" + fileContent;
-		}
-
-		let domainSelectorPairs = load_domains_and_selectors_from_tsv(fileContent);
-
-		const upsertApiUrl = 'api/upsert_dkim_record';
-		logmsg(`starting upload to ${upsertApiUrl}`);
-		for (const { domain, selector } of domainSelectorPairs) {
-			logmsg(`uploading ${domain} ${selector}`);
-			await axios.get(upsertApiUrl, { params: { domain, selector } })
-				.then(response => {
-					console.log('response.data: ', response.data);
-					logmsg(`response: ${response.data.message}`);
-				}).catch(error => {
-					console.log(error);
-					let data = error?.response?.data;
-					let message = `${error}` + (data ? ` - ${data}` : "");
-					throw message;
-				})
-		}
-	}
-
-	async function startStopButton() {
-		if (!started) {
-			setStarted(true);
-			try {
-				await uploadFile();
-				logmsg("upload complete");
-			}
-			catch (error) {
-				logmsg(`upload failed: ${error}`);
-			}
-			finally {
-				setStarted(false);
-			}
-		}
-	}
-
-	const startEnabled = selectedFile && !started;
-
 	return (
 		<div>
 			{(status == "authenticated" && session?.user?.email) &&
@@ -98,20 +26,7 @@ export default function Page() {
 					<button onClick={() => signOut()}>Sign out</button>
 				</div>
 			}
-			<p>
-				Add records to the database by providing a TSV file with domains and selectors.
-				This page will parse the file and add the records to the database via the <code>api/upsert_dkim_record</code> API.
-			</p>
-			<div>
-				<div>Select a file:</div>
-				<input type="file" onChange={fileSelectCallback} accept=".tsv,.txt" />
-			</div>
-			<p>
-				<button disabled={!startEnabled} onClick={startStopButton}>
-					{started ? "Running..." : "Start"}
-				</button>
-			</p>
-			<LogConsole log={log} setLog={setLog} />
+			<UploadTsv />
 		</div >
 	)
 }
