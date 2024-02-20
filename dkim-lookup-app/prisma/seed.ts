a8fee067b28f6f1b8ceec12a4433cf213f6d411d
Olof Andersson <olof.andersson@gmail.com>
2024-02-20 14:23:08 +0100

    add database seed script

---
+++ dkim-lookup-app/prisma/seed.ts
@@ -0,0 +1,42 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function randomHex(size: number) {
+	return [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('')
+};
+
+const domains = ["example.com", "example.org", "example.net", "mail.example.com"]
+const selectors = ["s1", "s2", "20240101"]
+
+async function main() {
+	for (const domain of domains) {
+		for (const selector of selectors) {
+			const dsp = await prisma.domainSelectorPair.create({
+				data: {
+					domain: domain,
+					selector: selector,
+				}
+			});
+			await prisma.dkimRecord.create({
+				data: {
+					domainSelectorPairId: dsp.id,
+					firstSeenAt: new Date(),
+					lastSeenAt: new Date(),
+					provenanceVerified: false,
+					value: `v=DKIM1; k=rsa; p=${randomHex(100)}`
+				}
+			});
+		}
+	}
+}
+
+main()
+	.then(async () => {
+		await prisma.$disconnect()
+	})
+	.catch(async (e) => {
+		console.error(e)
+		await prisma.$disconnect()
+		process.exit(1)
+	})
