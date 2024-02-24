24d34d8da06848636b6ad8eda53f2382959cd112
Olof Andersson <olof.andersson@gmail.com>
2024-02-24 11:47:04 +0100

    add statistics page

---
+++ dkim-lookup-app/src/app/statistics/page.tsx
@@ -0,0 +1,26 @@
+import { prisma } from "@/lib/db";
+
+export default async function Page() {
+
+	// https://github.com/prisma/prisma/issues/4228
+	type CountResult = [{ count: BigInt }]
+	let [uniqueDomainsCount] = await prisma.$queryRaw`SELECT COUNT(DISTINCT domain) FROM "DomainSelectorPair";` as CountResult;
+
+	let domainSelectorPairCount = await prisma.domainSelectorPair.count();
+	let dkimKeyCount = await prisma.dkimRecord.count();
+
+	return (
+		<div>
+			<h1>Statistics</h1>
+			<p>
+				Unique domains: {uniqueDomainsCount.count.toString()}
+			</p>
+			<p>
+				Domain/selector-pairs: {domainSelectorPairCount}
+			</p>
+			<p>
+				DKIM keys: {dkimKeyCount}
+			</p>
+		</div>
+	)
+}
