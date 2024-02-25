4dee69f719f509b33c4ea7bde6234344d7fe2920
Olof Andersson <olof.andersson@gmail.com>
2024-02-25 10:21:17 +0100

    move statistics to About page

--- dkim-lookup-app/src/app/about/page.tsx
+++ dkim-lookup-app/src/app/about/page.tsx
@@ -1,4 +1,14 @@
-export default function Page() {
+import { prisma } from "@/lib/db";
+import React from "react";
+
+export default async function Page() {
+
+	// https://github.com/prisma/prisma/issues/4228
+	type CountResult = [{ count: BigInt }]
+	let [uniqueDomainsCount] = await prisma.$queryRaw`SELECT COUNT(DISTINCT domain) FROM "DomainSelectorPair";` as CountResult;
+
+	let domainSelectorPairCount = await prisma.domainSelectorPair.count();
+	let dkimKeyCount = await prisma.dkimRecord.count();
 
 	return (
 		<div>
@@ -14,6 +24,17 @@ export default function Page() {
 			</p>
 			<p>
 				For each record, the site also generates an on-chain proof with <a href="https://witness.co/">Witness</a>, which functions as a data availability timestamp.
+			</p>
+
+			<h2>Statistics</h2>
+			<p>
+				Unique domains: {uniqueDomainsCount.count.toString()}
+			</p>
+			<p>
+				Domain/selector-pairs: {domainSelectorPairCount}
+			</p>
+			<p>
+				DKIM keys: {dkimKeyCount}
 			</p>
 		</div >
 	)
