c2d1ad5ab4575de2ffb4ae1dba0adebca8804daf
Olof Andersson <olof.andersson@gmail.com>
2024-02-27 13:15:37 +0100

    update privacy policy, fixes #45

--- dkim-lookup-app/src/app/privacy-policy/page.tsx
+++ dkim-lookup-app/src/app/privacy-policy/page.tsx
@@ -4,37 +4,41 @@ export default function Page() {
 		<div>
 			<h1>Privacy Policy</h1>
 
-			<h2>Privacy Policy for Proof of Email</h2>
-			<p>This Privacy Policy describes how DKIM Registry, operated by <strong>Proof of Email</strong> (hereinafter referred to as "we", "us", or "our"), collects, uses, and shares your personal information when you use our website located at <a href="https://registry.prove.email">registry.prove.email</a> (the "Website").</p>
-			<p><strong>Information We Collect:</strong></p>
+			<h2>Privacy policy for Proof of Email</h2>
+			<p>This privacy policy describes how DKIM Registry, operated by <strong>Proof of Email</strong> (hereinafter referred to as "we", "us", or "our"), accesses, uses, stores and shares your personal information when you use our website located at <a href="https://registry.prove.email">registry.prove.email</a> (the "website").</p>
+			<p><strong>Information we access:</strong></p>
 			<ul>
-				<li><strong>User Email Address:</strong> When you sign in with your Gmail account to contribute domains and selectors, we collect your email address solely for displaying it to you within the platform.</li>
-				<li><strong>Email Message Header Fields:</strong> With your consent, we access your email message metadata, specifically the header fields, to extract domains and selectors from the <code>DKIM-Signature</code> header. This information is used to build our archive of archived DKIM records.</li>
+				<li><strong>User email address:</strong> When you sign in with your Gmail account to contribute domains and selectors, we use your email address solely for displaying it to you within the platform.</li>
+				<li><strong>Email metadata:</strong> With your consent, we access your email message metadata, specifically the header fields, to extract domains and selectors from the <code>DKIM-Signature</code> header field.</li>
 			</ul>
-			<p><strong>How We Use Your Information:</strong></p>
+			<p><strong>Information we store:</strong></p>
 			<ul>
-				<li><strong>Contribution page:</strong> We use your email address solely for displaying it within the platform when you use the "Contribute" feature.</li>
-				<li><strong>Build DKIM Archive:</strong> We use the extracted domains and selectors from your email header fields to build a publicly accessible archive of historical DKIM records. This archive helps ensure email authentication and combat email spoofing.</li>
+				<li><strong>Domain and selector from the DKIM-Signature header field:</strong> For each email message in your email account, we store the domain (<code>d=</code>) and the selector part (<code>s=</code>) of the <code>DKIM-Signature</code> field. The data is used to build our archive of DKIM keys.</li>
 			</ul>
-			<p><strong>Information Sharing and Disclosure:</strong></p>
+			<p><strong>How we use your information:</strong></p>
+			<ul>
+				<li><strong>Displaying your email address:</strong> We use your email address for displaying it within the platform when you use the "Upload from Gmail" feature.</li>
+				<li><strong>Build an archive of DKIM keys:</strong> We use the extracted domains and selectors from your email header fields to build a publicly accessible archive of historical DKIM keys.</li>
+			</ul>
+			<p><strong>Information sharing and disclosure:</strong></p>
 			<ul>
 				<li>We do not share your personal information with any third parties except as required by law or to protect our rights and interests.</li>
-				<li>The archived DKIM records, containing domains and selectors (but not personal information), are publicly accessible on the Website.</li>
+				<li>The archived DKIM keys, containing domains and selectors (but not personal information), are publicly accessible on the website.</li>
 				<li>We may share anonymized or aggregated data with third parties for research or analytical purposes.</li>
 			</ul>
-			<p><strong>Data Security:</strong></p>
+			<p><strong>Data security:</strong></p>
 			<ul>
 				<li>We take reasonable steps to protect your personal information from unauthorized access, disclosure, alteration, or destruction. However, no internet transmission is completely secure, and we cannot guarantee the security of your information.</li>
 			</ul>
-			<p><strong>Your Choices:</strong></p>
+			<p><strong>Your choices:</strong></p>
 			<ul>
-				<li>You can choose not to contribute domains and selectors by not using the "Contribute" feature.</li>
+				<li>You can choose not to contribute domains and selectors by not using the "Upload from Gmail" feature.</li>
 				<li>You can revoke your consent for us to access your email message metadata at any time by managing your Gmail app permissions.</li>
 				<li>You can request to delete your user account by contacting us.</li>
 			</ul>
-			<p><strong>Changes to this Privacy Policy:</strong></p>
+			<p><strong>Changes to this privacy policy:</strong></p>
 			<ul>
-				<li>We may update this Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on the Website.</li>
+				<li>We may update this privacy policy from time to time. We will notify you of any changes by posting the new privacy policy on the website.</li>
 			</ul>
 		</div >
 	)
