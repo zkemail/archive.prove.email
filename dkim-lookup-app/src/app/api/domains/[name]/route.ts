f7d52cf7bcd98ce5d50f83913558c2dff1c70a72
Olof Andersson <olof.andersson@gmail.com>
2024-02-28 19:45:49 +0100

    Revert "add rate limit to api/domains endpoint"
    This reverts commit fc1a1c85cf670868871b202814f5d2893726ac5d.

--- dkim-lookup-app/src/app/api/domains/[name]/route.ts
+++ dkim-lookup-app/src/app/api/domains/[name]/route.ts
@@ -1,15 +1,5 @@
 import { findRecords } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
-import { Ratelimit } from "@upstash/ratelimit";
-import { Redis } from "@upstash/redis";
-
-const ratelimit = new Ratelimit({
-	redis: Redis.fromEnv(),
-	limiter: Ratelimit.slidingWindow(10, "10 s"),
-	analytics: true,
-	prefix: "dkimreg",
-});
-const ratelimitIdentifier = "api";
 
 export type DomainSearchResults = {
 	domain: string;
@@ -20,12 +10,6 @@ export type DomainSearchResults = {
 };
 
 export async function GET(_request: NextRequest, { params }: { params: { name: string } }) {
-
-	const ratelimitResponse = await ratelimit.limit(ratelimitIdentifier);
-	if (!ratelimitResponse.success) {
-		return NextResponse.json({ message: "Rate limit exceeded" }, { status: 429 });
-	}
-
 	try {
 		const domainName = params.name;
 		if (!domainName) {
