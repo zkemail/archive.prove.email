37cc995d382452d8adbbfb10aaabc4fd1ad7dd4f
Olof Andersson <olof.andersson@gmail.com>
2024-03-05 18:50:53 +0100

    gmp

--- dkim-lookup-app/src/app/api/gmail/route.ts
+++ dkim-lookup-app/src/app/api/gmail/route.ts
@@ -43,7 +43,7 @@ async function handleRequest(request: NextRequest) {
 		return new Response('Unauthorized. Sign in via api/auth/signin', { status: 401 });
 	}
 
-	const token = await getToken({ req: request	})
+	const token = await getToken({ req: request })
 	const oauth2Client = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, process.env.GOOGLE_REDIRECT_URI);
 	const gmail = google.gmail({ version: 'v1', auth: oauth2Client });
 
