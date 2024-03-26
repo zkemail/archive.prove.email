878dc6e2528d35d440e2e100d02d2900a44dcc08
Olof Andersson <olof.andersson@gmail.com>
2024-03-26 14:00:18 +0100

    revalidate statistics

--- dkim-lookup-app/src/app/about/page.tsx
+++ dkim-lookup-app/src/app/about/page.tsx
@@ -1,6 +1,8 @@
 import { prisma } from "@/lib/db";
 import React from "react";
 
+export const revalidate = 60;
+
 export default async function Page() {
 
 	// https://github.com/prisma/prisma/issues/4228
