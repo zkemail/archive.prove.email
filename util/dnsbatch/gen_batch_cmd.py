0a0e0dbd0db8301942be27e66b76ec4cce1f5ddf
Olof Andersson <olof.andersson@gmail.com>
2024-04-04 18:20:40 +0200

    tools

--- util/dnsbatch/gen_batch_cmd.py
+++ util/dnsbatch/gen_batch_cmd.py
@@ -1,16 +1,27 @@
 #!.venv/bin/python
 
 import os
+import sys
+
 
 def run():
+	output_dir = sys.argv[1]
+	print('set -e')
+	print('set -o xtrace')
 	for i in range(100):
 		input_file = f'mm{i:02d}'
-		output_file = f'dnsbatch/mm/{input_file}.out'
+		output_file = f'{output_dir}/{input_file}.out'
+		tmp_output_file = f'{input_file}.out.tmp'
 		if os.path.exists(output_file):
 			print(f'# {output_file} exists')
+			print()
 			continue
-		batch_cmd = f".venv/bin/modal run dsp_onetime_batch.py --domains-filename dnsbatch/mm/{input_file} --selectors-filename dnsbatch/merged.txt --no-sparse | grep '^DNS_BATCH_RESULT,' > {output_file}"
-		print(batch_cmd)
+		print(
+		    f".venv/bin/modal run dsp_onetime_batch.py --domains-filename dnsbatch/mm/{input_file} --selectors-filename dnsbatch/merged.txt --no-sparse | grep '^DNS_BATCH_RESULT,' > {tmp_output_file}"
+		)
+		print(f"mv {tmp_output_file} {output_file}")
+		print()
+
 
 if __name__ == "__main__":
 	run()
