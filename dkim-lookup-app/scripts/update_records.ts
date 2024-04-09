235e4bbc634749b575ed6f8b899cd42f387320d2
Olof Andersson <olof.andersson@gmail.com>
2024-04-09 09:33:25 +0200

    post processing, dsp_admin

--- dkim-lookup-app/scripts/update_records.ts
+++ dkim-lookup-app/scripts/update_records.ts
@@ -1,45 +1,65 @@
 import { readFileSync } from 'node:fs';
-import { addDomainSelectorPair } from '@/lib/utils_server';
+import { AddDspAdminRequest, AddDspAdminResponse } from '@/app/api/dsp_admin/route';
+import axios from 'axios';
 
 function print_usage() {
 	console.log('usage:');
-	console.log('  pnpm update_records RECORDS_FILE SOURCE');
+	console.log('  pnpm update_records API_URL RECORDS_FILE SOURCE_IDENTIFIER START_LINE');
+	console.log('    API_URL: The URL of the API endpoint to call');
 	console.log('    RECORDS_FILE: A .tsv file with domain and selector columns');
 	console.log('    SOURCE_IDENTIFIER: A string to identify the source of the records');
 	console.log('    START_LINE: The first line number (one-indexed) to process from the TSV file');
 	console.log('example:');
-	console.log('  pnpm update_records records.tsv batch_lookup_results 1');
+	console.log('  pnpm update_records http://localhost:3000/api/dsp_admin records.tsv batch_lookup_results 1');
 }
 
 async function main() {
 	const args = process.argv.slice(2);
-	if (args.length != 3) {
+	if (args.length != 4) {
 		print_usage();
 		process.exit(1);
 	}
-	const filename = args[0];
-	const sourceIdentifier = args[1];
-	const startLine = parseInt(args[2]);
+	const url = args[0];
+	const filename = args[1];
+	const sourceIdentifier = args[2];
+	const startLine = parseInt(args[3]);
 	console.log(`loading domains and selectors from ${filename}`);
 	const fileContent = readFileSync(filename, 'utf8');
 	const lines = fileContent.split('\n').map(line => line.trim()).filter(line => line);
 	console.log('fetching dkim records from dns');
+	let addedPairs = 0;
+
+
+	const config = {
+		headers: { Authorization: `Bearer ${process.env.CRON_SECRET}` }
+	};
+	console.log(`config: ${JSON.stringify(config)}`)
+
 	for (let lineNumber = startLine; lineNumber <= lines.length; lineNumber++) {
 		const line = lines[lineNumber - 1];
-		console.error(`processing line ${lineNumber} of ${lines.length}`);
+		console.log(`processing line ${lineNumber} of ${lines.length}`);
 		const [domain, selector] = line.split('\t');
 		if (!selector || !domain) {
-			console.error(`warning: line ${lineNumber}, selector or domain is empty`);
+			console.log(`warning: line ${lineNumber}, selector or domain is empty`);
 			continue;
 		}
-		console.log(`updating ${domain}, ${selector}`);
+
+		const dsp = { domain, selector };
+		const request = { sourceIdentifier, ...dsp };
 		try {
-			await addDomainSelectorPair(domain, selector, sourceIdentifier);
+			let response = await axios.post<AddDspAdminResponse>(url, request as AddDspAdminRequest, config);
+			if (response.data.added) {
+				console.log(`${JSON.stringify(dsp)} was added to the registry`);
+				addedPairs++;
+			}
 		}
-		catch (error) {
-			console.log(`error updating ${domain}, ${selector}: ${error}`);
+		catch (error: any) {
+			console.log(`error calling ${url}:`, error);
+			process.exit(1);
 		}
+
 	}
+	console.log(`added ${addedPairs} domain-selector pairs`);
 }
 
 main();
--- dkim-lookup-app/src/app/api/dsp_admin/route.ts
+++ dkim-lookup-app/src/app/api/dsp_admin/route.ts
@@ -2,11 +2,8 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { addDomainSelectorPair } from '@/lib/utils_server';
 import { z } from 'zod';
-import { headers } from "next/headers";
-import { RateLimiterMemory } from 'rate-limiter-flexible';
-import { checkRateLimiter } from '@/lib/utils';
 
-export type AddDspResponse = {
+export type AddDspAdminResponse = {
 	message: object;
 	added?: boolean;
 };
@@ -14,26 +11,22 @@ export type AddDspResponse = {
 const AddDspRequestSchema = z.object({
 	domain: z.string(),
 	selector: z.string(),
+	sourceIdentifier: z.string(),
 });
 
-export type AddDspRequest = z.infer<typeof AddDspRequestSchema>;
-
-const rateLimiter = new RateLimiterMemory({ points: 1200, duration: 360 });
+export type AddDspAdminRequest = z.infer<typeof AddDspRequestSchema>;
 
 export async function POST(request: NextRequest) {
-	try {
-		await checkRateLimiter(rateLimiter, headers(), 1);
+	const authHeader = request.headers.get('authorization');
+	if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+		return new Response('Unauthorized', { status: 401 });
 	}
-	catch (error: any) {
-		return NextResponse.json('Rate limit exceeded', { status: 429 });
-	}
-
 	try {
 		const body = await request.json();
 		const dsp = AddDspRequestSchema.parse(body);
-		let added = await addDomainSelectorPair(dsp.domain, dsp.selector, 'api');
+		let added = await addDomainSelectorPair(dsp.domain, dsp.selector, dsp.sourceIdentifier);
 		return NextResponse.json(
-			{ message: dsp, added } as AddDspResponse,
+			{ message: dsp, added } as AddDspAdminResponse,
 			{ status: 200 }
 		);
 	}
