ee78673ecaf203b26688388858190485e7fc1690
Olof Andersson <olof.andersson@gmail.com>
2024-04-09 13:14:50 +0200

    add api/dsp_admin endpoint and update update_records.ts to use it

--- dkim-lookup-app/scripts/update_records.ts
+++ dkim-lookup-app/scripts/update_records.ts
@@ -1,47 +1,64 @@
 import { readFileSync } from 'node:fs';
-import { load_domains_and_selectors_from_tsv } from '@/lib/utils';
-import { addDomainSelectorPair } from '@/lib/utils_server';
-
-abstract class Updater {
-	abstract update(domain: string, selector: string, sourceIdentifier: string): Promise<void>;
-}
-
-class PrismaUpdater extends Updater {
-	async update(domain: string, selector: string, sourceIdentifier: string) {
-		try {
-			await addDomainSelectorPair(domain, selector, sourceIdentifier);
-		}
-		catch (error) {
-			console.log(`error updating ${domain}, ${selector}: ${error}`);
-		}
-	}
-}
+import { AddDspAdminRequest, AddDspAdminResponse } from '@/app/api/dsp_admin/route';
+import axios from 'axios';
 
 function print_usage() {
 	console.log('usage:');
-	console.log('  pnpm update_records RECORDS_FILE SOURCE');
+	console.log('  pnpm update_records API_URL RECORDS_FILE SOURCE_IDENTIFIER START_LINE');
+	console.log('    API_URL: The URL of the API endpoint to call');
 	console.log('    RECORDS_FILE: A .tsv file with domain and selector columns');
 	console.log('    SOURCE_IDENTIFIER: A string to identify the source of the records');
+	console.log('    START_LINE: The first line number (one-indexed) to process from the TSV file');
 	console.log('example:');
-	console.log('  pnpm update_records records.tsv batch_lookup_results');
+	console.log('  pnpm update_records http://localhost:3000/api/dsp_admin records.tsv batch_lookup_results 1');
 }
 
 async function main() {
 	const args = process.argv.slice(2);
-	if (args.length != 2) {
+	if (args.length != 4) {
 		print_usage();
 		process.exit(1);
 	}
-	let filename = args[0];
-	let sourceIdentifier = args[1];
+	const url = args[0];
+	const filename = args[1];
+	const sourceIdentifier = args[2];
+	const startLine = parseInt(args[3]);
 	console.log(`loading domains and selectors from ${filename}`);
-	const updater = new PrismaUpdater();
-	const fileContents = readFileSync(filename, 'utf8');
-	let domainSelectorPairs = load_domains_and_selectors_from_tsv(fileContents);
+	const fileContent = readFileSync(filename, 'utf8');
+	const lines = fileContent.split('\n').map(line => line.trim()).filter(line => line);
 	console.log('fetching dkim records from dns');
-	for (const p of domainSelectorPairs) {
-		await updater.update(p.domain, p.selector, sourceIdentifier);
+	let addedPairs = 0;
+
+
+	const config = {
+		headers: { Authorization: `Bearer ${process.env.CRON_SECRET}` }
+	};
+	console.log(`config: ${JSON.stringify(config)}`)
+
+	for (let lineNumber = startLine; lineNumber <= lines.length; lineNumber++) {
+		const line = lines[lineNumber - 1];
+		console.log(`processing line ${lineNumber} of ${lines.length}`);
+		const [domain, selector] = line.split('\t');
+		if (!selector || !domain) {
+			console.log(`warning: line ${lineNumber}, selector or domain is empty`);
+			continue;
+		}
+
+		const request = { domain, selector, sourceIdentifier };
+		try {
+			let response = await axios.post<AddDspAdminResponse>(url, request as AddDspAdminRequest, config);
+			if (response.data.added) {
+				addedPairs++;
+			}
+			console.log(`response for ${JSON.stringify(request)}, added=${response.data.added}`);
+		}
+		catch (error: any) {
+			console.log(`error calling ${url}:`, error);
+			process.exit(1);
+		}
+
 	}
+	console.log(`added ${addedPairs} domain-selector pairs`);
 }
 
 main();
