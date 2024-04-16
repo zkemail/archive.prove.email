1e9205958ecbef2c73ab5d75828246e0ab9dd362
Olof Andersson <olof.andersson@gmail.com>
2024-04-16 18:28:00 +0200

    catch error in add_dsps_from_stdin.ts

--- dkim-lookup-app/scripts/add_dsps_from_stdin.ts
+++ dkim-lookup-app/scripts/add_dsps_from_stdin.ts
@@ -8,8 +8,13 @@ async function main() {
 			console.log(`warning: selector or domain is empty`);
 			continue;
 		}
-		let added = await addDomainSelectorPair(domain, selector, 'scraper');
-		console.log(`domain: ${domain}, selector: ${selector}, added: ${added}`);
+		try {
+			let added = await addDomainSelectorPair(domain, selector, 'scraper');
+			console.log(`domain: ${domain}, selector: ${selector}, added: ${added}`);
+		}
+		catch (error: any) {
+			console.error(`add_dsps_from_stdin.ts error: ${error}`);
+		}
 	}
 }
 
