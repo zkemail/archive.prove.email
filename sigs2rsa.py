a021517365e101368875efe8160205562e74aed1
Olof Andersson <olof.andersson@gmail.com>
2024-04-24 16:41:55 +0200

    convert to python3 and add import sage.all

--- sigs2rsa.py
+++ sigs2rsa.py
@@ -1,14 +1,15 @@
 # https://blog.ploetzli.ch/2018/calculating-an-rsa-public-key-from-two-signatures/
 
 import binascii, hashlib
+from sage.all import *
 
 def pkcs1_padding(size_bytes, hexdigest, hashfn):
     oid = {hashlib.sha256: '608648016503040201'}[hashfn]
-    result = '06' + ("%02X" % (len(oid)/2)) + oid + '05' + '00'
-    result = '30' + ("%02X" % (len(result)/2)) + result
+    result = '06' + ("%02X" % (len(oid)//2)) + oid + '05' + '00'
+    result = '30' + ("%02X" % (len(result)//2)) + result
     
-    result = result + '04' + ("%02X" % (len(hexdigest)/2)) + hexdigest
-    result = '30' + ("%02X" % (len(result)/2)) + result
+    result = result + '04' + ("%02X" % (len(hexdigest)//2)) + hexdigest
+    result = '30' + ("%02X" % (len(result)//2)) + result
     
     result = '0001' + ('ff' * int(size_bytes - 3 - len(result)/2) ) + '00' + result
     return result
@@ -18,7 +19,7 @@ def hash_pad(size_bytes, data, hashfn):
     return pkcs1_padding(size_bytes, hexdigest, hashfn)
 
 def message_sig_pair(size_bytes, data, signature, hashfn=hashlib.sha256):
-    return ( Integer('0x' + hash_pad(size_bytes, data, hashfn)), Integer('0x' + binascii.hexlify(signature)) )
+    return ( Integer('0x' + hash_pad(size_bytes, data, hashfn)), Integer('0x' + binascii.hexlify(signature).decode('utf-8')) )
 
 def find_n(*filenames):
     data_raw = []
