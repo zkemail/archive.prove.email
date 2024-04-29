f7f836c8f82fc59688a0e5fc43e0c29e48f090f1
Olof Andersson <olof.andersson@gmail.com>
2024-04-29 16:46:16 +0200

    remove unused

--- mbox_extract_signatures.py
+++ mbox_extract_signatures.py
@@ -10,27 +10,7 @@ from dataclasses import dataclass
 
 # https://russell.ballestrini.net/quickstart-to-dkim-sign-email-with-python/
 
-privkey = """-----BEGIN PRIVATE KEY-----
-MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAKjfGUdZ23sjVsIc
-8btWpFGsdtVxmlfZ8g47RW26vi5+c1jjKYEIhTnhtRLJD+9S18GgdfQzMXJiBoxU
-NYWd+6tC0ANl0iDnq+VcmyQ5rEOFvdwVtqkgShPf6hSI5shSZ5JDh6f3OI7O6PtY
-KnernKhOapgHm1dPgrYNnJRf8uPHAgMBAAECgYAErzuqSQnXnqFXfSoPglXIljot
-rZsUeM1IK8i/RIDmFUfp3VNXav8XHfXB8aXpg6jMjED5Zzol7CY2Wlepvzot+IFD
-Py5dshvz7D6Af9qqffcR5czvXRwu4qud9RcdQxAjBoyZagigjIb8iUYlrzEq3TtG
-rY0DxR8RaUErTUNKcQJBANHAwoQEScBe4Ee9tSCqSxWEaXR0LQ6ZW7c/RchaGKEi
-dx+O8I0+8P/BavJZp+B4fSkpcavwwzmWGZe4S7aBDOMCQQDOGtXNBKJsEU9nUnyQ
-0TM1tRoTe8XZbbFHk1gWS5ShlVuJaRdcK2HxA+3ckQdeB/9v3b9yhX/KDFw0MM7w
-DMbNAkB4qBx8mobePPVg71S41JzaZM/QqF+ezUL/90qqBIG0d0H1CmF/rpwtUtK1
-VDOIoWbF/cwgrx9uCdTw/Je5BSUnAkEAngKszxUygK90tL3kihIYtKA3caB+uocC
-VuF7svwW4xpipyJOqncIC4L7pRH7F/gBuX3D/MSkh/6Z4dlZjWf+MQJBALcmALEn
-R9PFTctb7138xCv7KHTVYJR/IhVOvhlVsISNDw2cn850ryrrGnbT6RUjYk2w6VGQ
-lcD1AW8sD6HEpo0=
------END PRIVATE KEY-----
-"""
-
-
 def decode_dkim_header_field(dkimData: str):
-    #print(f'decode_dkim_header_field: {dkimData}', file=sys.stderr)
     # decode a DKIM-Signature header field such as "v=1; a=rsa-sha256; d=example.net; s=brisbane;"
     # to a dictionary such as {'v': '1', 'a': 'rsa-sha256', 'd': 'example.net', 's': 'brisbane'}
     tagValuePairStrings = list(map(lambda x: x.strip(), dkimData.split(';')))
@@ -53,11 +33,7 @@ class MsgInfo:
 
 def write_msg_info(msgInfo: MsgInfo, outDir: str, dskey: str, index: int):
     outDirDsp = os.path.join(outDir, dskey)
-    #print(f'  {msgInfo.fullMsg}', file=sys.stderr)
-    #print(f'  signedData: {msgInfo.signedData}', file=sys.stderr)
-    #print(f'  signature: {msgInfo.signature}', file=sys.stderr)
     outDirDspMsgN = os.path.join(outDirDsp, str(index))
-    #print(f'  outDirDspMsgN: {outDirDspMsgN}', file=sys.stderr)
     if not os.path.exists(outDirDspMsgN):
         os.makedirs(outDirDspMsgN)
     with open(os.path.join(outDirDspMsgN, 'data'), 'wb') as f:
@@ -78,7 +54,6 @@ def main():
     outDir = args.output_dir
     if not os.path.exists(outDir):
         os.makedirs(outDir)
-    # Create .gitignore file in outDir
     gitignore_path = os.path.join(outDir, '.gitignore')
     with open(gitignore_path, 'w') as f:
         f.write('*\n')
@@ -105,18 +80,10 @@ def main():
             tags = decode_dkim_header_field(field)
             domain = tags['d']
             selector = tags['s']
-            #print(f'domain: {domain}, selector: {selector}', file=sys.stderr)
-            # includeHeaders = tags['h'].split(':')
-            # includeHeaders = list(map(lambda x: x.strip(), includeHeaders))
-            # if 'received' in map(lambda x: x.lower(), includeHeaders):
-            #     #print('received in includeHeaders not supported, skipping', file=sys.stderr)
-            #     continue
-            #canonicalize = tags['c']
             signAlgo = tags['a']
             if signAlgo != 'rsa-sha256' and signAlgo != 'rsa-sha1':
                 print(f'skip signAlgo that is not rsa-sha256 or rsa-sha1: {signAlgo}', file=sys.stderr)
                 continue
-            #canonicalizeTuple = list(map(lambda x: x.encode(), canonicalize.split('/')))
             bodyHash = tags.get('bh', None)
             if not bodyHash:
                 print('body hash not found, skipping', file=sys.stderr)
@@ -139,22 +106,11 @@ def main():
                 print(f'WARNING: UnicodeEncodeError: {e}', file=sys.stderr)
                 continue
 
-            # d.sign(selector.encode(),
-            #        domain.encode(),
-            #        privkey.encode(),
-            #        canonicalize=canonicalizeTuple,
-            #        include_headers=list(map(lambda x: x.encode(), includeHeaders)),
-            #        length=False,
-            #        preknownBodyHash=bodyHash.encode(),
-            #        infoOut=infoOut)
-
-            from dkimpy.dkim.dnsplug import get_txt_dnspython
             try:
-                d.verify(0, dnsfunc=get_txt_dnspython, infoOut=infoOut)
+                d.verify(0, infoOut=infoOut)
             except dkim.ValidationError as e:
                 print(f'WARNING: ValidationError: {e}', file=sys.stderr)
                 continue
-            #print('infoOut:', infoOut, file=sys.stderr)
             body_hash_mismatch = infoOut.get('body_hash_mismatch', False)
             try:
                 signed_data = infoOut['signed_data']
