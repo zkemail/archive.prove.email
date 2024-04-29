e958dd6670eb253ea815cf366fbc4395a852618b
Olof Andersson <olof.andersson@gmail.com>
2024-04-29 17:40:58 +0200

    directory structure fix

--- solve_msg_pairs.py
+++ solve_msg_pairs.py
@@ -4,33 +4,39 @@ import os
 import subprocess
 import sys
 
+
+def subdirs(d):
+    return [f for f in os.listdir(d) if os.path.isdir(os.path.join(d, f))]
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('rootdir')
     args = parser.parse_args()
     rootdir = args.rootdir
-    for d in os.listdir(rootdir):
-        domainDir = os.path.join(rootdir, d)
-        if os.path.isdir(domainDir):
-            selectorDirs = os.listdir(domainDir)
-            for s in selectorDirs:
-                dspDir = os.path.join(domainDir, s)
-                print(dspDir, file=sys.stderr)
-                msgDirs = os.listdir(dspDir)
-                msgDirA = os.path.join(dspDir, msgDirs[0])
-                msgDirB = os.path.join(dspDir, msgDirs[1])
-                cmd = [
-                    "docker",
-                    "run",
-                    "--rm",
-                    "--mount",
-                    f"type=bind,source={os.getcwd()},target=/app",
-                    "--workdir=/app",
-                    "sagemath:latest",
-                    "sage",
-                    "sigs2rsa.py",
-                    msgDirA + "/data",
-                    msgDirB + "/data",
-                ]
-                print(" ".join(cmd), file=sys.stderr)
-                subprocess.run(cmd)
+    for d in subdirs(rootdir):
+        domainPath = os.path.join(rootdir, d)
+        for s in subdirs(domainPath):
+            dspPath = os.path.join(domainPath, s)
+            print(dspPath, file=sys.stderr)
+            msgDirs = subdirs(dspPath)
+            if len(msgDirs) < 2:
+                print(f'expected at least 2 message directories in {dspPath}', file=sys.stderr)
+                continue
+            msgDirA = os.path.join(dspPath, msgDirs[0])
+            msgDirB = os.path.join(dspPath, msgDirs[1])
+            cmd = [
+                "docker",
+                "run",
+                "--rm",
+                "--mount",
+                f"type=bind,source={os.getcwd()},target=/app",
+                "--workdir=/app",
+                "sagemath:latest",
+                "sage",
+                "sigs2rsa.py",
+                msgDirA + "/data",
+                msgDirB + "/data",
+            ]
+            print(" ".join(cmd), file=sys.stderr)
+            subprocess.run(cmd)
