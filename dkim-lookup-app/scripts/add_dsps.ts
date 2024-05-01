7d75d9f67591d7c58877d942c13e647b3893dc16
Olof Andersson <olof.andersson@gmail.com>
2024-05-01 07:18:27 +0200

    add load from file functionality in add_dsps.ts

--- dkim-lookup-app/scripts/add_dsps.ts
+++ dkim-lookup-app/scripts/add_dsps.ts
@@ -1,19 +1,38 @@
 import { addDomainSelectorPair } from "@/lib/utils_server";
 import { createInterface } from "node:readline"
+import { readFileSync } from 'node:fs';
+
+
+async function process_line(line: string) {
+	const [domain, selector] = line.split('\t');
+	if (!selector || !domain) {
+		console.log(`warning: selector or domain is empty`);
+		return;
+	}
+	try {
+		let added = await addDomainSelectorPair(domain, selector, 'scraper');
+		console.log(`domain: ${domain}, selector: ${selector}, added: ${added}`);
+	}
+	catch (error: any) {
+		console.error(`add_dsps_from_stdin.ts error: ${error}`);
+	}
+}
 
 async function main() {
-	for await (const line of createInterface({ input: process.stdin })) {
-		const [domain, selector] = line.split('\t');
-		if (!selector || !domain) {
-			console.log(`warning: selector or domain is empty`);
-			continue;
+	const args = process.argv.slice(2);
+	if (args.length == 1) {
+		const filename = args[0];
+		console.log(`reading DSPs from file: ${filename}`);
+		const fileContent = readFileSync(filename, 'utf8');
+		const lines = fileContent.split('\n').map(line => line.trim()).filter(line => line);
+		for (let line of lines) {
+			await process_line(line);
 		}
-		try {
-			let added = await addDomainSelectorPair(domain, selector, 'scraper');
-			console.log(`domain: ${domain}, selector: ${selector}, added: ${added}`);
-		}
-		catch (error: any) {
-			console.error(`add_dsps_from_stdin.ts error: ${error}`);
+	}
+	else {
+		console.log('reading DSPs from stdin');
+		for await (const line of createInterface({ input: process.stdin })) {
+			await process_line(line);
 		}
 	}
 }
