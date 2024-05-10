8651dd6473f26e4eb5768a1d491b6707578595c4
Olof Andersson <olof.andersson@gmail.com>
2024-05-10 09:24:18 +0200

    update check_pubkey_solver_results.ts

--- dkim-lookup-app/scripts/check_pubkey_solver_results.ts
+++ dkim-lookup-app/scripts/check_pubkey_solver_results.ts
@@ -4,14 +4,6 @@ import { parseDkimTagList } from "@/lib/utils";
 import { readFileSync } from "node:fs";
 
 
-async function process_line(line: string) {
-	const [domain, selector, key] = line.split('\t');
-	if (!selector || !domain || !key) {
-		throw new Error(`selector or domain is empty`);
-	}
-	return [domain, selector, key];
-}
-
 export async function findKnownKeys(domain: string, selector: string) {
 	let records = await prisma.dkimRecord.findMany({
 		where: {
@@ -49,11 +41,27 @@ export async function findKnownKeys(domain: string, selector: string) {
 	return result;
 }
 
+type SolveResult = {
+	tvl: string,
+	sources: string[],
+};
+
+type DspAndKeys = {
+	domain: string,
+	selector: string,
+	solve_results: SolveResult[],
+};
+
+type DspToKeysMap = {
+	[dsp_index: string]: DspAndKeys,
+};
+
 async function main() {
 	console.log('reading DSPs from stdin');
-	let previouslyUnseenKeysFound = [];
-	let solvedKeysNotMatchingArchive = [];
-	let solvedKeysMatchingArchive = [];
+	let previouslyUnseenKeysFound: [string, string, string, string][] = [];
+	let solvedKeysNotMatchingArchive: [string, string, string, string][] = [];
+	let solvedKeysMatchingArchive: [string, string, string, string][] = [];
+	let notSolved: [string, string, string][] = [];
 
 	const args = process.argv.slice(2);
 	if (args.length == 1) {
@@ -61,40 +69,83 @@ async function main() {
 		console.log(`reading DSPs from file: ${filename}`);
 		const fileContent = readFileSync(filename, 'utf8');
 		const lines = fileContent.split('\n').map(line => line.trim()).filter(line => line);
+		let dspToKeysMap: DspToKeysMap = {};
 		for (let i = 0; i < lines.length; i++) {
 			const line = lines[i];
-			const [domain, selector, solved_key_tag_list] = await process_line(line);
-			const key_der_format = parseDkimTagList(solved_key_tag_list).p;
+			const [dsp_index, domain, selector, solved_key_tvl, source1, source2] = line.split('\t');
+			if (!dspToKeysMap[dsp_index]) {
+				dspToKeysMap[dsp_index] = { domain, selector, solve_results: [] };
+			}
+			dspToKeysMap[dsp_index].solve_results.push({ tvl: solved_key_tvl, sources: [source1, source2] });
+		}
+
+		for (let dsp_index in dspToKeysMap) {
+
+			const { domain, selector, solve_results } = dspToKeysMap[dsp_index];
+			const sources = solve_results.map(key => key.sources).join(', ');
+			let validKeys = []
+			for (let r of solve_results) {
+				if (r.tvl !== '-') {
+					validKeys.push(r);
+				}
+			}
+			if (validKeys.length == 0) {
+				notSolved.push([domain, selector, sources]);
+			}
+
+			if (solve_results.map(key => key.tvl).includes('-') && validKeys.length > 0) {
+				console.log(`results for some but not all message pairs for ${domain}, ${selector}: ${JSON.stringify(solve_results)}`);
+				continue;
+			}
+
+			const allKeysAreSame = validKeys.every(key => key === validKeys[0]);
+			if (!allKeysAreSame) {
+				console.log(`differing keys for ${domain}, ${selector}: ${JSON.stringify(validKeys)}`);
+			}
+
+			const solved_key = solve_results[0];
+			if (solved_key.tvl === '-') {
+				//console.log(`skip DSP with empty key for domain: ${domain}, selector: ${selector}`);
+				continue;
+			}
+
+			const key_der_format = parseDkimTagList(solved_key.tvl).p;
 			const knownKeys = await findKnownKeys(domain, selector);
 			if (knownKeys.length == 0) {
-				previouslyUnseenKeysFound.push([domain, selector, solved_key_tag_list]);
+				previouslyUnseenKeysFound.push([domain, selector, solved_key.tvl, sources]);
 			}
 			else {
 				if (knownKeys.includes(key_der_format)) {
-					solvedKeysMatchingArchive.push([domain, selector, solved_key_tag_list]);
+					solvedKeysMatchingArchive.push([domain, selector, solved_key.tvl, sources]);
 				}
 				else {
-					solvedKeysNotMatchingArchive.push([domain, selector, solved_key_tag_list]);
+					solvedKeysNotMatchingArchive.push([domain, selector, solved_key.tvl, sources]);
 				}
 			}
-			console.log(`line ${i + 1} of ${lines.length}, previouslyUnseenKeysFound: ${previouslyUnseenKeysFound.length}, solvedKeysMatchingArchive: ${solvedKeysMatchingArchive.length}, solvedKeysNotMatchingArchive: ${solvedKeysNotMatchingArchive.length}`);
+			console.log(`${dsp_index}, previouslyUnseenKeysFound: ${previouslyUnseenKeysFound.length}, solvedKeysMatchingArchive: ${solvedKeysMatchingArchive.length}, solvedKeysNotMatchingArchive: ${solvedKeysNotMatchingArchive.length}`);
 		}
 
 
 		console.log();
 		console.log(`solved keys for which there are no keys in the archive for the corresponding DSP: ${previouslyUnseenKeysFound.length}`);
-		for (let [domain, selector, solved_key] of previouslyUnseenKeysFound) {
-			console.log(`domain: ${domain}, selector: ${selector}, solved_key: ${solved_key}`);
+		for (let [domain, selector, result, sources] of previouslyUnseenKeysFound) {
+			console.log(`${domain}\t${selector}\t${result}\t${sources}`);
 		}
 		console.log();
 		console.log(`solved keys which match a key in the archive: ${solvedKeysMatchingArchive.length}`);
-		for (let [domain, selector] of solvedKeysMatchingArchive) {
-			console.log(`domain: ${domain}, selector: ${selector}`);
+		for (let [domain, selector, key, sources] of solvedKeysMatchingArchive) {
+			console.log(`${domain}\t${selector}\t${key}\t${sources}`);
 		}
 		console.log();
 		console.log(`solved keys for which there are keys in the archive for the corresponding DSP, but no match: ${solvedKeysNotMatchingArchive.length}`);
-		for (let [domain, selector, solved_key] of solvedKeysNotMatchingArchive) {
-			console.log(`domain: ${domain}, selector: ${selector}, solved_key: ${solved_key}`);
+		for (let [domain, selector, result, sources] of solvedKeysNotMatchingArchive) {
+			console.log(`${domain}\t${selector}\t${result}\t${sources}`);
+		}
+
+		console.log();
+		console.log(`DSPs for which no keys were solved: ${notSolved.length}`);
+		for (let [domain, selector, sources] of notSolved) {
+			console.log(`${domain}\t${selector}\t${sources}`);
 		}
 	}
 }
