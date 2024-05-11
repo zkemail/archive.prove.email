61676477df5d32cf334b255d3601b7923d59fe75
Olof Andersson <olof.andersson@gmail.com>
2024-05-11 12:39:31 +0200

    add DecodeException

--- util/common.py
+++ util/common.py
@@ -1,3 +1,7 @@
+class DecodeException(Exception):
+	pass
+
+
 def decode_dkim_header_field(dkimData: str):
 	# decode a DKIM-Signature header field such as "v=1; a=rsa-sha256; d=example.net; s=brisbane;"
 	# to a dictionary such as {'v': '1', 'a': 'rsa-sha256', 'd': 'example.net', 's': 'brisbane'}
@@ -6,9 +10,11 @@ def decode_dkim_header_field(dkimData: str):
 	for s in tagValuePairStrings:
 		if not s:
 			continue
-		key, value = s.split('=', 1)
+		try:
+			key, value = s.split('=', 1)
+		except ValueError:
+			raise DecodeException(f'Error decoding DKIM tag-value pair: {s}')
 		key = key.strip()
 		value = value.strip()
 		res[key] = value
 	return res
-
