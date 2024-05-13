9701d719c273f65299bff1d025b3dd60486f2e0b
Olof Andersson <olof.andersson@gmail.com>
2024-05-13 10:47:40 +0200

    add Content-Security-Policy

--- dkim-lookup-app/next.config.js
+++ dkim-lookup-app/next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const cspValue = [
+	"default-src 'self'",
+	"style-src 'self' 'unsafe-inline'",
+	"frame-ancestors 'none'",
+	"script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+	"img-src 'self' https://authjs.dev/ data:", // https://authjs.dev/ for images during the login flow
+	"connect-src 'self' https://*.alchemy.com", // https://*.alchemy.com used by Witness
+]
+
+const nextConfig = {
+	async headers() {
+		return [{
+			source: '/(.*)',
+			headers: [
+				{
+					key: 'Content-Security-Policy',
+					value: cspValue.join('; '),
+				},
+			],
+		}]
+	},
+}
 
 module.exports = nextConfig
