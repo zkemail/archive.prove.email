8d009003ec5cbaea11499997a463d65a0f28bc4b
Olof Andersson <olof.andersson@gmail.com>
2024-05-14 13:51:12 +0200

    add multiple dns records

--- dkim-lookup-app/src/lib/utils_server.ts
+++ dkim-lookup-app/src/lib/utils_server.ts
@@ -4,7 +4,7 @@ import { generateWitness } from "./generateWitness";
 import { DnsDkimFetchResult, SourceIdentifier } from "./utils";
 import dns from 'dns';
 
-async function refreshKey(dsp: DomainSelectorPair) {
+async function refreshKeysFromDns(dsp: DomainSelectorPair) {
 	let now = new Date();
 	let oneHourAgo = new Date(now.getTime() - 1000 * 60 * 60);
 	if (!dsp.lastRecordUpdate || dsp.lastRecordUpdate < oneHourAgo) {
@@ -37,34 +37,35 @@ export async function addDomainSelectorPair(domain: string, selector: string, so
 	});
 	if (dsp) {
 		console.log(`found domain/selector pair ${dspToString(dsp)}`);
-		refreshKey(dsp);
+		refreshKeysFromDns(dsp);
 		return false;
 	}
-	let dkimDnsRecord = await fetchDkimDnsRecord(domain, selector);
-	if (!dkimDnsRecord) {
+	let records = await fetchDkimDnsRecord(domain, selector);
+	if (records.length === 0) {
 		console.log(`no dkim dns record found for ${selector}, ${domain}`);
 		return false;
 	}
+	console.log(`found ${records.length} dkim dns records for ${selector}, ${domain}, adding DSP and records`);
+
 	let newDsp = await prisma.domainSelectorPair.create({
 		data: {
 			domain,
 			selector,
 			sourceIdentifier,
-			lastRecordUpdate: dkimDnsRecord.timestamp,
+			lastRecordUpdate: new Date(),
 			records: {
-				create: {
-					value: dkimDnsRecord.value,
-					firstSeenAt: dkimDnsRecord.timestamp,
-					lastSeenAt: dkimDnsRecord.timestamp,
+				create: [...records.map(record => ({
+					value: record.value,
+					firstSeenAt: record.timestamp,
+					lastSeenAt: record.timestamp,
 					provenanceVerified: false
-				}
+				}))]
 			}
 		},
 		include: {
 			records: true
 		}
 	})
-
 	newDsp.records.forEach(record => {
 		generateWitness(newDsp, record);
 	});
@@ -72,33 +73,31 @@ export async function addDomainSelectorPair(domain: string, selector: string, so
 	return true;
 }
 
-export async function fetchDkimDnsRecord(domain: string, selector: string): Promise<DnsDkimFetchResult | null> {
+export async function fetchDkimDnsRecord(domain: string, selector: string): Promise<DnsDkimFetchResult[]> {
 	const resolver = new dns.promises.Resolver({ timeout: 2500 });
 	const qname = `${selector}._domainkey.${domain}`;
-	let response;
+	let records;
 	try {
-		response = await resolver.resolve(qname, 'TXT');
+		records = (await resolver.resolve(qname, 'TXT')).map(record => record.join(''));
 	}
 	catch (error) {
 		console.log(`error fetching ${qname}: ${error}`);
-		return null;
+		return [];
 	}
-	if (response.length === 0) {
-		console.log(`warning: no records found for ${qname}`);
-		return null;
+	console.log(`found: ${records.length} records for ${qname}`);
+	let result = [];
+	for (let record of records) {
+		console.log(`record: ${record}`);
+		console.log(`found dns record for ${qname}`);
+		const dkimRecord: DnsDkimFetchResult = {
+			selector,
+			domain,
+			value: record,
+			timestamp: new Date(),
+		};
+		result.push(dkimRecord);
 	}
-	if (response.length > 1) {
-		console.log(`warning: > 1 record found for ${qname}, using first one`);
-	}
-	console.log(`found dns record for ${qname}`);
-	const dkimData = response[0].join('');
-	const dkimRecord: DnsDkimFetchResult = {
-		selector,
-		domain,
-		value: dkimData,
-		timestamp: new Date(),
-	};
-	return dkimRecord;
+	return result
 }
 
 /**
@@ -106,29 +105,28 @@ export async function fetchDkimDnsRecord(domain: string, selector: string): Prom
  */
 export async function fetchAndStoreDkimDnsRecord(dsp: DomainSelectorPair) {
 	console.log(`fetching ${dsp.selector}._domainkey.${dsp.domain} from dns`);
-	let dkimDnsRecord = await fetchDkimDnsRecord(dsp.domain, dsp.selector);
-	if (!dkimDnsRecord) {
-		return;
-	}
-	let dkimRecord = await prisma.dkimRecord.findFirst({
-		where: {
-			domainSelectorPair: dsp,
-			value: dkimDnsRecord.value
-		},
-	});
-
-	if (dkimRecord) {
-		console.log(`record already exists: ${recordToString(dkimRecord)} for domain/selector pair ${dspToString(dsp)}, updating lastSeenAt to ${dkimDnsRecord.timestamp}`);
-		await prisma.dkimRecord.update({
-			where: { id: dkimRecord.id },
-			data: { lastSeenAt: dkimDnsRecord.timestamp }
+	let dnsRecords = await fetchDkimDnsRecord(dsp.domain, dsp.selector);
+	for (let dnsRecord of dnsRecords) {
+		let dbRecord = await prisma.dkimRecord.findFirst({
+			where: {
+				domainSelectorPair: dsp,
+				value: dnsRecord.value
+			},
 		});
-	}
-	else {
-		dkimRecord = await createDkimRecord(dsp, dkimDnsRecord);
-	}
 
-	if (!dkimRecord.provenanceVerified) {
-		generateWitness(dsp, dkimRecord);
+		if (dbRecord) {
+			console.log(`record already exists: ${recordToString(dbRecord)} for domain/selector pair ${dspToString(dsp)}, updating lastSeenAt to ${dnsRecord.timestamp}`);
+			await prisma.dkimRecord.update({
+				where: { id: dbRecord.id },
+				data: { lastSeenAt: dnsRecord.timestamp }
+			});
+		}
+		else {
+			dbRecord = await createDkimRecord(dsp, dnsRecord);
+		}
+
+		if (!dbRecord.provenanceVerified) {
+			generateWitness(dsp, dbRecord);
+		}
 	}
 }
