5688ffe30486993658cf395ce15e8d28bb5a3b4c
Olof Andersson <olof.andersson@gmail.com>
2024-06-06 13:59:08 +0200

    formatting

--- src/util/pubkey_finder/common.py
+++ src/util/pubkey_finder/common.py
@@ -3,42 +3,42 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class Dsp:
-    domain: str
-    selector: str
+	domain: str
+	selector: str
 
-    def __init__(self, domain: str, selector: str):
-        object.__setattr__(self, 'domain', domain.lower())
-        object.__setattr__(self, 'selector', selector.lower())
+	def __init__(self, domain: str, selector: str):
+		object.__setattr__(self, 'domain', domain.lower())
+		object.__setattr__(self, 'selector', selector.lower())
 
 
 @dataclass
 class MsgInfo:
-    signedData: bytes
-    signature: bytes
-    source: str
-    date: str
+	signedData: bytes
+	signature: bytes
+	source: str
+	date: str
 
 
 # https://stackoverflow.com/a/2212923/961254
 def gen_primes():
-    D: dict[int, list[int]] = {}
-    q = 2
-    while True:
-        if q not in D:
-            yield q
-            D[q * q] = [q]
-        else:
-            for p in D[q]:
-                D.setdefault(p + q, []).append(p)
-            del D[q]
-        q += 1
+	D: dict[int, list[int]] = {}
+	q = 2
+	while True:
+		if q not in D:
+			yield q
+			D[q * q] = [q]
+		else:
+			for p in D[q]:
+				D.setdefault(p + q, []).append(p)
+			del D[q]
+		q += 1
 
 
 def first_n_primes(n: int) -> list[int]:
-    x: set[int] = set()
-    y = 0
-    a = gen_primes()
-    while y < n:
-        x |= set([next(a)])
-        y += 1
-    return list(sorted(x))
+	x: set[int] = set()
+	y = 0
+	a = gen_primes()
+	while y < n:
+		x |= set([next(a)])
+		y += 1
+	return list(sorted(x))
--- src/util/pubkey_finder/lib/util.py
+++ src/util/pubkey_finder/lib/util.py
@@ -4,13 +4,13 @@ import time
 
 @dataclass
 class ProgressReporter:
-    total: int
-    current: int
-    last_printed_time: float = 0
+	total: int
+	current: int
+	last_printed_time: float = 0
 
-    def increment(self):
-        self.current += 1
-        if time.time() - self.last_printed_time < 0.2:
-            return
-        self.last_printed_time = time.time()
-        print(f'\r{self.current}/{self.total}', end='', flush=True)
+	def increment(self):
+		self.current += 1
+		if time.time() - self.last_printed_time < 0.2:
+			return
+		self.last_printed_time = time.time()
+		print(f'\r{self.current}/{self.total}', end='', flush=True)
