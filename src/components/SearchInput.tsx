3f5669a309ee4df6b6adda21b0b59567c0832181
Aryan Singh <krish7664@gmail.com>
2024-09-04 21:52:08 +0530

    Fixes slow autocomplete

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -35,6 +35,7 @@ export const SearchInput: React.FC<SearchFormProps> = ({ domainQuery }) => {
 				disablePortal
 				onInputChange={inputChanged}
 				onChange={onChange}
+				filterOptions={(x) => x}				
 				options={searchResults}
 				sx={{ width: 300 }}
 				freeSolo
