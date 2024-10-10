705b536547029ba2f044d84ded1f1e05a7abff93
Aryan <krish7664@gmail.com>
2024-10-10 06:42:18 +0530

    Adds loading state for fetching data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,39 @@
-import { DomainSearchResults } from '@/components/DomainSearchResults';
-import { SearchInput } from '@/components/SearchInput';
-import { findKeysPaginated } from './actions';
-import { parseDkimTagList } from '@/lib/utils';
+import DomainSearchResults from "@/components/DomainSearchResults";
+import { SearchInput } from "@/components/SearchInput";
+import Link from "next/link";
 
-function dkimValueHasPrivateKey(dkimValue: string): boolean {
-	return !!parseDkimTagList(dkimValue).p;
-}
-
-export default async function Home({ searchParams }: {
-	searchParams: { [key: string]: string | string[] | undefined }
-}) {
-	const domainQuery = searchParams?.domain?.toString();
-	let records = domainQuery ? await findKeysPaginated(domainQuery, null) : []
-	records = records.filter((record) => dkimValueHasPrivateKey(record.value));
-
-	return (
-		<div style={{ display: 'flex', minHeight: '100vh', flexDirection: 'column', alignItems: 'center' }}>
-			<h2 style={{ padding: '2rem' }}>
-				<a href='/' className='defaultcolor'>DKIM Archive</a>
-			</h2>
-			<SearchInput domainQuery={domainQuery} />
-			<DomainSearchResults initialRecords={records} domainQuery={domainQuery} />
-
-			<div style={{ textAlign: 'center', marginTop: '5rem', fontSize: '0.8rem' }}>
-				<hr style={{ width: '50%', margin: '1rem auto', borderTop: '1px solid black' }} />
-				<div><a href="about">About</a> this site</div>
-				<div>Visit the project on <a href="https://github.com/zkemail/archive.prove.email">GitHub</a></div>
-				<div>Visit <a href="https://prove.email/">Proof of Email</a></div>
-				<div><a href="contribute">Contribute</a> to the archive</div>
-				<div>Explore the <a href="api-explorer">API</a></div>
-				<div>Read the <a href="privacy-policy">Privacy policy</a></div>
-			</div>
-		</div>
-	)
+export default function Home({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
+  const domainQuery = searchParams?.domain?.toString();
+  return (
+    <div style={{ display: "flex", minHeight: "100vh", flexDirection: "column", alignItems: "center" }}>
+      <h2 style={{ padding: "2rem" }}>
+        <Link href="/" className="defaultcolor">
+          DKIM Archive
+        </Link>
+      </h2>
+      <SearchInput domainQuery={domainQuery} />
+      <DomainSearchResults domainQuery={domainQuery} />
+      <div style={{ textAlign: "center", marginTop: "5rem", fontSize: "0.8rem" }}>
+        <hr style={{ width: "50%", margin: "1rem auto", borderTop: "1px solid black" }} />
+        <div>
+          <a href="about">About</a> this site
+        </div>
+        <div>
+          Visit the project on <a href="https://github.com/zkemail/archive.prove.email">GitHub</a>
+        </div>
+        <div>
+          Visit <a href="https://prove.email/">Proof of Email</a>
+        </div>
+        <div>
+          <a href="contribute">Contribute</a> to the archive
+        </div>
+        <div>
+          Explore the <a href="api-explorer">API</a>
+        </div>
+        <div>
+          Read the <a href="privacy-policy">Privacy policy</a>
+        </div>
+      </div>
+    </div>
+  );
 }
